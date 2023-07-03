e2269f25b5988d0b2112e0fe4e695ac0e54eeaf3
Johan Cortes <jako12287@gmail.com>
2023-07-03 08:08:11 -0500

    TD-07 Create CustomButton component

--- src/components/CustomButton/index.tsx
+++ src/components/CustomButton/index.tsx
@@ -1,17 +1,16 @@
 import { ReactNode } from "react";
 import { useNavigate } from "react-router-dom";
-import { Button } from "reactstrap";
 
 interface Props {
     children: ReactNode;
-    URI: string
+    URI?: string
 }
-const CustomButtom: React.FC<Props> = ({ children, URI }) => {
+const CustomButtom: React.FC<Props> = ({ children, URI = "" }) => {
     const navigate = useNavigate()
     return (
-        <Button onClick={() => navigate(URI)} className="w-60 h-10 rounded-full bg-secondary text-primary text-button font-SemiBold hover:bg-secondaryHover shadow-btn">
+        <button onClick={() => navigate(URI)} className="w-60 h-10 rounded-full bg-secondary text-primary text-button font-SemiBold hover:bg-secondaryHover shadow-btn">
             {children}
-        </Button >
+        </button>
     );
 };
 
